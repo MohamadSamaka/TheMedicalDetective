696c8e73d66422b55cd9ffccf339a9a2f802add6
thexjoker-desktop <mhmdkais20@gmail.com>
2023-06-15 19:34:22 +0300

    now the chatbot models are loaded on the the project kickoff so the time for the user waiting for it's diagnosis is reduced by a lot

--- core/chatbot/apps.py
+++ core/chatbot/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.chatbot'
+
+    def ready(self):
+        from core.model_managment.src.models.diagnoser import DiagnoserModel
+        from core.model_managment.src.models.NER import NERModel
+        self.ner_model, self.tokenizer = NERModel.Model.load_model(), NERModel.Tokenizer.load_tokenizer()
+        self.diagnoser_model = DiagnoserModel.Model.load_model()
