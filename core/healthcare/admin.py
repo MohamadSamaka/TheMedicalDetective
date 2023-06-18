031621458868d79a6b4bb6b0d27572b10e7f9ba4
thexjoker-desktop <mhmdkais20@gmail.com>
2023-06-19 02:18:16 +0300

    this is my adminpage for the doctors i registered it

--- core/healthcare/admin.py
+++ core/healthcare/admin.py
@@ -1,6 +1,21 @@
+from django.contrib import admin
+from core.core.admin import BaseAdminSite
+from core.my_admin.admin import my_admin_site
 from .models import UsersMeicalRecord, Symptoms, Diseases
-from core.my_admin.views import my_admin_site
+from core.booking.models import Booking
+from core.chatbot.models import DiagnosticCorrector
 
-my_admin_site.register(UsersMeicalRecord)
+
+class MyDoctorSite(BaseAdminSite):
+    site_header = 'Doctor Dashboard'
+    site_title = 'Doctor Dashboard'
+    index_title = 'Doctor Dashboard'
+    
+
+# my_admin_site.register(UsersMeicalRecord)
 my_admin_site.register(Symptoms)
 my_admin_site.register(Diseases)
+
+my_doctor_site = MyDoctorSite(name="adminpage-doctor")
+# my_doctor_site.register(Booking)
+# my_doctor_site.register(DiagnosticCorrector)
