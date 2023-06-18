9e1f9ae1c09bf49d0b34d7221e98d8c970eaf533
thexjoker-desktop <mhmdkais20@gmail.com>
2023-06-19 02:12:06 +0300

    moved admin site from views after i understood how admin's works, and realized that its not a view

--- core/my_admin/admin.py
+++ core/my_admin/admin.py
@@ -1,30 +1,66 @@
 from django.contrib import admin
 from .models import CustomUser, CustomUserManager
-from .views import my_admin_site
 from django.apps import AppConfig
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+from django.apps import apps
+from django.contrib.auth import get_user_model
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse, path
+# from core.authentication.src.views.login import MyLoginView
+from core.core.admin import BaseAdminSite
 
 
+class MyAdminSite(BaseAdminSite):
+    site_header = 'Admin Dashboard'
+    site_title = 'Admin Dashboard'
+    index_title = 'Admin Dashboard'
+    # logout_template = 'client/pages/home.html'
+    def get_urls(self):
+        urls = super().get_urls()
 
-# class MyAppConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'Healthcare'
+        # Remove the change password URL
+        # urls = [url for url in urls if not url.name == 'logout']
+    
+        custom_urls = [
+            path('train-NER/', self.admin_view(self.NER_trainer), name='train_ner_model'),
+            path('train-diagnoser/', self.admin_view(self.diagnose_trainer), name='train_diagnoser_model'),
+        ]
 
-#     def ready(self):
-#         # Define your custom group
-#         group_name = 'My Custom Group'
-#         group, created = Group.objects.get_or_create(name=group_name)
+        return custom_urls + urls
 
-#         # Add permissions to the group
-#         model_permissions = Permission.objects.filter(content_type__app_label='myapp', content_type__model='mymodel')
-#         for permission in model_permissions:
-#             if permission.codename.startswith('add') or permission.codename.startswith('change'):
-#                 group.permissions.add(permission)
+    
+        
+    def NER_trainer(self, request):
+        from core.model_managment.src.models.NER import NERModel
+        trainer = NERModel.Trainer()
+        trainer.train_model()
+        trainer.save_model()
+        trainer.save_tokenizer()
+        return HttpResponse("Training Done")
+    
+    def diagnose_trainer(self, request):
+        from core.model_managment.src.models.diagnoser import DiagnoserModel
+        trainer = DiagnoserModel.Trainer()
+        trainer.train_model()
+        trainer.save_model()
+        return HttpResponse("Training Done")
 
-#         # Register your custom group
-#         self.register_group(my_admin_site, group)
 
-my_admin_site.register(Group)
+# Create an instance of the custom AdminSite class
+my_admin_site = MyAdminSite(name='adminpage-admin')
+
+
+default_apps = ['auth', 'contenttypes', 'sessions', 'admin', 'messages', 'staticfiles']
+
+# Get a list of all installed models
+all_models = apps.get_models()
+
+# Iterate over the models and register them if they belong to the default apps
+for model in all_models:
+    if model._meta.app_label in default_apps and not my_admin_site.is_registered(model):
+        my_admin_site.register(model)
+
 my_admin_site.register(CustomUser)
-# my_admin_site.register(CustomUserManager)
 
