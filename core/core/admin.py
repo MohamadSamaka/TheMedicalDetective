0b3e693ad9dbf2749e1c44da7e6b3c5d6ca99a5f
thexjoker-desktop <mhmdkais20@gmail.com>
2023-06-19 02:15:09 +0300

    made sure that the neither the admin not the doctor can access each other's admin pages

--- core/core/admin.py
+++ core/core/admin.py
@@ -1,5 +1,26 @@
-from django.apps import apps
+# from core.my_admin.admin import my_admin_site
+from django.contrib.auth.models import Group
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
+from django.contrib.admin import AdminSite
+from core.authentication.src.views.login import MyLoginView
+from core.core.src.utls.helpers import user_belongs_to_group
 from .models import City
-from ..my_admin.views import my_admin_site
 
+class BaseAdminSite(AdminSite):
+    login_template = 'authentication/pages/log-in.html'
+
+    def index(self, request, extra_context=None):
+        if request.user.is_authenticated:
+            if user_belongs_to_group(request.user, "Doctor"):
+                if not request.path.startswith(reverse('adminpage-doctor:index')):
+                    return HttpResponseRedirect(reverse('adminpage-doctor:index'))
+            else:
+                if not request.path.startswith(reverse('adminpage-admin:index')):
+                    return HttpResponseRedirect(reverse('adminpage-admin:index'))
+
+        return super().index(request, extra_context)
+
+
+from core.my_admin.admin import my_admin_site
 my_admin_site.register(City)
