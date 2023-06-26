8c4492fce5dd6c4b7b26735c93f6ededd94f1f8f
thexjoker-desktop <mhmdkais20@gmail.com>
2023-06-26 15:05:57 +0300

    added the diagnoser related model

---
+++ core/chatbot_models_manager/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Diagnoser(models.Model):
+    model_name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    
+    def __str__(self):
+        return self.model_name
+
+class NER(models.Model):
+    model_name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    tokenizer_name = models.CharField(
+        max_length=50,
+        unique=True,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self):
+        return self.model_name
