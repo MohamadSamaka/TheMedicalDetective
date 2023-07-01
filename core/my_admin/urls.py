ce0848d6ae220f7186208f6599ef6f65d9957306
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2023-07-02 00:57:41 +0300

    changes it from path to repath

--- core/my_admin/urls.py
+++ core/my_admin/urls.py
@@ -1,9 +1,9 @@
 from .admin import my_admin_site
 from core.healthcare.admin import my_doctor_site
-from django.urls import path
+from django.urls import re_path
 
 
 urlpatterns = [
-    path('admin/', my_admin_site.urls, name="admin-dashboard"),
-    path('doctor/', my_doctor_site.urls, name="doctor-dashboard"),
+    re_path(r'^admin/', my_admin_site.urls, name="admin-dashboard"),
+    re_path(r'^doctor/', my_doctor_site.urls, name="doctor-dashboard"),
 ]
