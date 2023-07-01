a1bd5522c5c1ca42a66234131c7148f57619a351
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2023-07-02 01:00:37 +0300

    added the new middleware with it's __init__.py file

---
+++ core/core/src/middlewares/admin_page_redirector.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from ..utls.helpers import user_belongs_to_group, redirect_to_user_or_doc_page
+
+class AdminDoctorMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            if user_belongs_to_group(request.user, "User"):
+                    return self.get_response(request)
+            if user_belongs_to_group(request.user, "Doctor"):
+                if not request.path.startswith(reverse('adminpage-doctor:index')):
+                    return redirect_to_user_or_doc_page(request.user)
+            else:
+                if not request.path.startswith(reverse('adminpage-admin:index')):
+                    return redirect_to_user_or_doc_page(request.user)
+        
+        return self.get_response(request)
