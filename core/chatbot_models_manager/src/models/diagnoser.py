532548e71995a5a9d12652dd17b5e51c74d69074
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2023-07-02 00:55:03 +0300

    edited the diagnoser a little so it can adapt to the new changes of celery and the way im letting user choose the structure of the model

--- core/chatbot_models_manager/src/models/diagnoser.py
+++ core/chatbot_models_manager/src/models/diagnoser.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.conf import settings
 import tensorflow as tf
 import pandas as pd
@@ -5,18 +6,16 @@ import numpy as np
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
-def send_progress_update(logs, num):
+
+def send_progress_update(logs):
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
         "train",
         {
-            # 'type': 'send_progress_update',
             'type': 'progress_update',
             'info': {
                 'loss': logs['loss'],
                 'accuracy': logs['accuracy'],
-                'val_loss': logs['val_loss'],
-                'num': num
             },
         }
     )
@@ -54,18 +53,19 @@ class SymptomsListNotFound(Exception):
 
 
 class DiagnoserModel:
-    training_data_fname = "default_training.csv"
-    testing_data_fname = "default_testing.csv"
+    # training_data_fname = "default_training.csv"
+    # testing_data_fname = "default_testing.csv"
     iterations = 100
     dense1_n_neurons = 64
     dense2_n_neurons = 32
 
-    def pre_proccessing(training_data, testing_data):
+    def pre_proccessing(training_file, testing_file):
         # encode training labels
-        training = pd.read_csv(settings.DATASETS_DIR / 'diagnoser'/ training_data)
-        DiagnoserModel.cols = training.columns[:-1]
-        x = training[DiagnoserModel.cols].values
-        y = training['prognosis'].values
+        # training = pd.read_csv(settings.DATASETS_DIR / 'diagnoser'/ training_data)
+        training_data = pd.read_csv(training_file)
+        DiagnoserModel.cols = training_data.columns[:-1]
+        x = training_data[DiagnoserModel.cols].values
+        y = training_data['prognosis'].values
         # map strings to numbers
         DiagnoserModel.unique_labels = list(set(y))
         label_map = {label: i for i, label in enumerate(DiagnoserModel.unique_labels)}
@@ -75,10 +75,13 @@ class DiagnoserModel:
         x_train, y_train = x[:split], y[:split]
         x_validate, y_validate= x[split:], y[split:]
 
-        testing = pd.read_csv(settings.DATASETS_DIR / 'diagnoser'/ testing_data)
-        x_test = testing[DiagnoserModel.cols].values
-        y_test = testing['prognosis'].values
+        # testing = pd.read_csv(settings.DATASETS_DIR / 'diagnoser'/ testing_data)
+        testing_data = pd.read_csv(testing_file)
+        x_test = testing_data[DiagnoserModel.cols].values
+        y_test = testing_data['prognosis'].values
         y_test = np.array([label_map[label] for label in y_test])
+        testing_file.seek(0)  
+        training_file.seek(0)  
         
         return (x_train, y_train), (x_validate, y_validate), (x_test, y_test)
 
@@ -86,17 +89,17 @@ class DiagnoserModel:
             self.model.save_model(model_name)
 
     class Trainer:
-        def __init__(self, training_data_fname = None, testing_data_fname = None, dense1_n_neurons = None , dense2_n_neurons = None, iterations = None):
-            self.initialize_defaults(training_data_fname, testing_data_fname, dense1_n_neurons, dense2_n_neurons, iterations)
-            self.training_data, self.validation_data, self.testing_data = DiagnoserModel.pre_proccessing(DiagnoserModel.training_data_fname, DiagnoserModel.testing_data_fname)
+        def __init__(self, training_file, testing_file, dense1_n_neurons , dense2_n_neurons, iterations):
+            self.initialize_defaults(dense1_n_neurons, dense2_n_neurons, iterations)
+            self.training_data, self.validation_data, self.testing_data = DiagnoserModel.pre_proccessing(training_file, testing_file)
             DiagnoserModel.input_shape = self.training_data[0].shape[1]
             self.model = DiagnoserModel.Model()
 
-        def initialize_defaults(self, training_data_fname, testing_data_fname, dense1_n_neurons, dense2_n_neurons, iterations):
-            if training_data_fname:
-                DiagnoserModel.training_data_fname = training_data_fname
-            if testing_data_fname:
-                DiagnoserModel.testing_data_fname = testing_data_fname
+        def initialize_defaults(self, dense1_n_neurons, dense2_n_neurons, iterations):
+            # if training_file:
+            #     DiagnoserModel.training_file = training_file
+            # if testing_file:
+            #     DiagnoserModel.testing_file = testing_file
             if dense1_n_neurons:
                 DiagnoserModel.dense1_n_neurons = dense1_n_neurons
             if dense2_n_neurons:
@@ -104,22 +107,8 @@ class DiagnoserModel:
             if iterations:
                 DiagnoserModel.iterations = iterations
 
-        def train_model(self):
-            # from channels.layers import get_channel_layer
-            # progress = 0
-            # for i in range(10):
-            #     progress += 10
-
-            #     # Send progress update to all connected consumers
-            #     channel_layer = get_channel_layer()
-            #     await channel_layer.group_send(
-            #         "train",
-            #         {
-            #             'type': 'send_progress_update',
-            #             'progress': progress,
-            #         }
-            #     )
-            self.model.train_model(self.training_data, self.validation_data)
+        def train_model(self, user_id):
+            self.model.train_model(user_id, self.training_data, self.validation_data)
             # self.model.train_model()
 
         def save_model(self, model_name="default_model"):
@@ -183,10 +172,6 @@ class DiagnoserModel:
 
             normalized_symps = DiagnoserModel.Model.normalize_symptoms(extracted_symptoms, original_symptoms)
             encoded_symptoms = DiagnoserModel.Model.onehot_encoded_symptoms(normalized_symps, original_symptoms)
-            # for symp in extracted_symptoms:
-            #     normalized_symps = DiagnoserModel.Model.normalize_symptoms(symp, symptoms)
-            #     index = symptoms[normalized_symps]
-            #     encoded_symptoms[index] = 1
             prediction = model.predict(np.array(encoded_symptoms).reshape((1, len(original_symptoms)))).argmax()
             return id_to_diease[prediction], normalized_symps
         
@@ -213,57 +198,48 @@ class DiagnoserModel:
                 normalized_symps_list.append(closest_normalization)
             return normalized_symps_list
 
-        def train_model(self, training_data, validation_data):
-            import threading
-            import asyncio
-
+        def train_model(self, user_id, training_data, validation_data):
             self.build_complie_model()
+            accuracy = None
+            
             class LossHistoryCallback(tf.keras.callbacks.Callback):
-                def __init__(self):
-                    self.num = 0
                 def on_epoch_end(self, epoch, logs=None):
-                    # thread = threading.Thread(target=self.run_async, args=(logs,))
-                    # thread.start()
-                    self.num+=1
-                    send_progress_update(logs, self.num)
-
-                # def run_async(self, logs):
-                #     loop = asyncio.new_event_loop()
-                #     asyncio.set_event_loop(loop)
-                #     loop.run_until_complete(send_progress_update(logs))
-                    
+                    accuracy = logs['accuracy']
+                    send_progress_update(logs)
+                    cancel_state = cache.get(user_id).get('cancel_flag')
+                    if cancel_state:
+                        self.model.stop_training = True       
+           
             loss_history_callback = LossHistoryCallback()
+            self.model_accuracy = accuracy
             self.model.fit(*training_data, epochs=DiagnoserModel.iterations, validation_data=validation_data, callbacks=[loss_history_callback])
-            return self.model
-        
-        # async def test(self):
-        #     from channels.layers import get_channel_layer
-        #     progress = 0
-        #     for i in range(10):
-        #         progress += 10
-        #         channel_layer = get_channel_layer()
-        #         await channel_layer.group_send(
-        #             "train",
-        #             {
-        #                 'type': 'send_progress_update',
-        #                 'progress': progress,
-        #             }
-        #         )
 
         def save_model(self, model_name):
             import pickle
+            import json
+            from pathlib import Path
             try:
+                model_path = settings.PROTECTED_MEDIA_ABSOLUTE_URL / Path(model_name)
                 symptoms_map = DiagnoserModel.cols.to_series().reset_index(drop=True).to_dict()
                 id_to_symptom = dict(zip(symptoms_map.values(), symptoms_map.keys()))
                 disease_to_id = {id: disease for id, disease in enumerate(DiagnoserModel.unique_labels)}
-                data = {
+                data_to_pikcle = {
                     'model': self.model,
                     'diseases': disease_to_id,
                     'symptoms': id_to_symptom,
                 }
-                with open(settings.MODELS_DIR / 'diagnoser' / model_name, 'wb') as f:
-                    pickle.dump(data, f)
-                    # pickle.dump(self.model, f)
+                
+                model_info = {
+                    'iterations': DiagnoserModel.iterations ,
+                    'accuracy': self.model_accuracy,
+                    'dense1_n_neurons' : DiagnoserModel.dense1_n_neurons,
+                    'dense2_n_neurons' : DiagnoserModel.dense2_n_neurons,
+                }
+                
+                with open(model_path / Path(f'{model_name}.model'), 'wb') as f:
+                    pickle.dump(data_to_pikcle, f)
+                with open(model_path / Path(f'{model_name}.json'), 'w') as f:
+                    json.dump(model_info, f)
                 return True
             except Exception as e:
                 print('Model has not been saved')
