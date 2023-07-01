a2da5e4339974d364447e42ab3cce053adfcc615
TheXJoKeR71 <mhmd_dragon1@hotmail.com>
2023-07-02 01:00:09 +0300

    added a new helper function which delets the dir with it's contents

--- core/core/src/utls/helpers.py
+++ core/core/src/utls/helpers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import Group
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.cache import cache
+import shutil
 
 def user_belongs_to_group(user, group_name):
     try:
@@ -13,3 +15,12 @@ def redirect_to_user_or_doc_page(user):
     if user_belongs_to_group(user, "Doctor"):
         return HttpResponseRedirect(reverse('adminpage-doctor:index'))
     return HttpResponseRedirect(reverse('adminpage-admin:index'))
+
+def delete_dir_with_contents_if_canceled(path, user_id):
+    if cache.get(user_id).get('cancel_flag'):
+        if path.is_dir():
+            shutil.rmtree(path)
+        cache.delete(user_id)
+        return True
+    return False
+    
