49d201498621e7a3669b95362ff70e201c6e0bcf
thexjoker-desktop <mhmdkais20@gmail.com>
2023-07-03 05:20:44 +0300

    added the new nerAdmin

--- core/chatbot_models_manager/admin.py
+++ core/chatbot_models_manager/admin.py
@@ -1,6 +1,7 @@
 from core.my_admin.admin import my_admin_site
 from .src.admin_pages.diagnoser_admin import DiagnoserAdmin
-from .models import Diagnoser
+from .src.admin_pages.ner_admin import NERAdmin
+from .models import Diagnoser, NER
 
 my_admin_site.register(Diagnoser, DiagnoserAdmin)
-# my_admin_site.register(NER)
+my_admin_site.register(NER, NERAdmin)
