cbc8b8d3dce31a92f873a4439ee15024352ed3a1
thexjoker-desktop <mhmdkais20@gmail.com>
2023-07-03 05:19:44 +0300

    added the new model

--- core/chatbot/models.py
+++ core/chatbot/models.py
@@ -40,3 +40,17 @@ class DiagnosticCorrector(models.Model):
 
     def __str__(self):
         return str(self.bot_diagnosis.subject)
+    
+class ChatbotSettings(models.Model):
+    used_diagnoser_model = models.OneToOneField(
+        'chatbot_models_manager.Diagnoser',
+        on_delete=models.CASCADE,
+    )
+
+    used_ner_model = models.OneToOneField(
+        'chatbot_models_manager.NER',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{str(self.used_diagnoser_model)} | {str(self.used_ner_model)}"
