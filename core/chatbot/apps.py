51bc20d110b4d6ec47c1c108d55ab101e155bf93
thexjoker-desktop <mhmdkais20@gmail.com>
2023-07-03 05:19:03 +0300

    re-set the chatbot model loaders, now they load the selected model by the admin

--- core/chatbot/apps.py
+++ core/chatbot/apps.py
@@ -10,7 +10,9 @@ class ChatbotConfig(AppConfig):
         if 'runserver' in sys.argv:
             from core.chatbot_models_manager.src.models.diagnoser import DiagnoserModel
             from core.chatbot_models_manager.src.models.NER import NERModel
-            # self.ner_model, self.tokenizer = NERModel.Model.load_model(), NERModel.Tokenizer.load_tokenizer()
-            # self.diagnoser_model = DiagnoserModel.Model.load_model()
-            self.ner_model, self.tokenizer = None, None
-            self.diagnoser_model = None
+            from core.chatbot.models import ChatbotSettings
+            model = ChatbotSettings.objects.first()
+            diagnoser_model_name = model.used_diagnoser_model.model_name
+            ner_model_name = model.used_ner_model.model_name
+            self.ner_model, self.tokenizer = NERModel.Model.load_model(ner_model_name), NERModel.Tokenizer.load_tokenizer(ner_model_name)
+            self.diagnoser_model = DiagnoserModel.Model.load_model(diagnoser_model_name)
