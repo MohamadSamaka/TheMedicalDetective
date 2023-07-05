429dbd7614f3fe21fecac878791438f9ff4526a7
thexjoker-desktop <mhmdkais20@gmail.com>
2023-07-05 07:24:52 +0300

    created the yes/no navigating buttons, and made sure that rendering the recomanded doctor name is optional, and added the even listeres and their function to control the buttons and as usual some clenaing

--- core/statics/js/pages/chatbot.js
+++ core/statics/js/pages/chatbot.js
@@ -1,17 +1,36 @@
 const userInput = document.getElementById('user-input')
 const chatLog = document.getElementById('chatlog');
 const parser = new DOMParser();
-const processingMessageSkeleton = `<div class="mssg new loading badge badge-primary">
-                                                <div class="loading-message">
-                                                    <span class="spinner-grow spinner-grow-sm" role="status"></span>
-                                                    <span class="spinner-grow spinner-grow-sm" role="status"></span>
-                                                    <span class="spinner-grow spinner-grow-sm" role="status"></span>
-                                                </div>
+const processingMessageSkeleton = `
+                                    <div class="mssg new loading badge badge-primary">
+                                            <div class="loading-message">
+                                                <span class="spinner-grow spinner-grow-sm" role="status"></span>
+                                                <span class="spinner-grow spinner-grow-sm" role="status"></span>
+                                                <span class="spinner-grow spinner-grow-sm" role="status"></span>
                                             </div>
-                                            `
+                                    </div>
+                                `
+
+const navigationButtons = `
+<div class="mssg new">
+    <p class="mssg-content">Would you like to book for this doctor?</p>
+    <button type="button" id="yes-btn" class="btn btn-primary rounded-pill">Yes</button>
+    <button type="button" id="not-btn" class="btn btn-dark rounded-pill">No</button>
+</div>
+
+`                
+
 
 const processingMessageHtmlDoc = parser.parseFromString(processingMessageSkeleton, 'text/html');
 const processingMessageElm = processingMessageHtmlDoc.querySelector('.mssg');
+
+
+
+const processingNavigationButtonsHtmlDoc = parser.parseFromString(navigationButtons, 'text/html');
+const navigationButtonsElm = processingNavigationButtonsHtmlDoc.querySelector('.mssg');
+
+const noButton = navigationButtonsElm.querySelector('#not-btn');
+const yesButton = navigationButtonsElm.querySelector('#yes-btn');
 
 
 function adjustInputHeight() {
@@ -21,14 +40,10 @@ function adjustInputHeight() {
 
 
 function generateMssgElm(content, personal=true) {
-    // send_case(input)
     const mssgSkeleton = `<div class="mssg new ${personal? "personal-message align-self-end": ""}"><p class="mssg-content m-0"></p></div>`;
-    // const skeleton = '<div class="mssg new personal-message"><p class="mssg-content m-0"></p></div>';
     const mssgHtmlDoc = parser.parseFromString(mssgSkeleton, 'text/html');
     const mssgElm = mssgHtmlDoc.querySelector('.mssg');
     mssgElm.querySelector('.mssg-content').innerHTML = content.replaceAll('\n', '<br>');
-    // let chatboxFixerElm = chatLog.querySelector('#chatbox-fixer')
-    // chatLog.insertBefore(mssgElm, chatboxFixerElm);
     return mssgElm
 }
 
@@ -39,7 +54,7 @@ function generateList(elm, items){
 }
 
 
-function generateDiagnosisMssg(symptoms, disease, purifications){
+function generateDiagnosisMssg(symptoms, disease, purifications, recomanded_doctor_name){
     const mssgSkeleton = `
         <div class="mssg new response-message">
             <div class="mssg-content m-0">
@@ -47,10 +62,15 @@ function generateDiagnosisMssg(symptoms, disease, purifications){
                 <ul id="symptoms-list">
                 </ul>
                 <h5>Diagnosis: </h5>
-                <p id="diagnosis" class="ps-3"></p>
+                <p id="diagnosis" class="ps-3">${disease}</p>
                 <h5>Purifications: </h5>
                 <ul id="purifications-list">
                 </ul>
+                ${
+                    recomanded_doctor_name ? `
+                    <h5><b>Recomanded Doctor:</b></h5>
+                    <p id="recomanded-doctor" class="ps-3">${recomanded_doctor_name}</p>` : ''
+                }
                 <h5><b>Cautions!</b></h5>
                 <p>
                     Please be aware that our symptom checker is intended for informational 
@@ -67,8 +87,6 @@ function generateDiagnosisMssg(symptoms, disease, purifications){
     const mssgElm = mssgHtmlDoc.querySelector('.mssg.response-message');
     const symptomsList = mssgElm.querySelector('#symptoms-list')
     const purificaionsList = mssgElm.querySelector('#purifications-list')
-    const diagnosisMssg = mssgElm.querySelector('#diagnosis')
-    diagnosisMssg.innerHTML = disease
     generateList(symptomsList, symptoms)
     generateList(purificaionsList, purifications)
     return mssgElm
@@ -142,19 +160,25 @@ function send_case(userCase){
         };
     }).then(data => {
         let loadingMssgElm = chatLog.querySelector('.mssg.loading')
-        console.log(data)
-        console.log(data.result.purifications)
+        doctorSuggestionExists = 'recomanded_doctor' in data.result
         result = generateDiagnosisMssg(
             data.result.symptoms,
             data.result.diagnosis,
             data.result.purifications,
+            doctorSuggestionExists? data.result.recomanded_doctor: '',
         )
+        if(doctorSuggestionExists)
+            setTimeout(()=> {
+                chatLog.appendChild(navigationButtonsElm)
+                chatLog.scrollTop = chatLog.scrollHeight;   
+            }
+            ,500);
+       
         loadingMssgElm.replaceWith(result)
         chatLog.scrollTop = chatLog.scrollHeight;   
     }).catch(err => {
         err.response.json().then(data => {
             let loadingMssgElm = chatLog.querySelector('.mssg.loading')
-            // result = generateMssgElm("Too few symptoms extracted, please provide me with more information.", false)
             result = generateMssgElm(data.message, false)
             loadingMssgElm.replaceWith(result)
             chatLog.scrollTop = chatLog.scrollHeight;   
@@ -165,6 +189,18 @@ function send_case(userCase){
 }
 
 
+function detatchRedirectQuestionMessage(){
+    $(navigationButtonsElm).addClass('reverse-animation');
+    $(navigationButtonsElm).one('animationend', function() {
+        $(navigationButtonsElm).remove();
+        $(navigationButtonsElm).removeClass('reverse-animation');
+    });
+}
+
+
 
 userInput.addEventListener('input', adjustInputHeight);
 userInput.addEventListener('keydown', sendMssg);
+
+noButton.addEventListener('click', detatchRedirectQuestionMessage);
+yesButton.addEventListener('click', ()=> window.location.href = '/booking/');
